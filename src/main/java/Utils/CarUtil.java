725dea6154e3d5c6b6272b2e3768e6f2c99522e3
김수윤 <darecaga1@gamil.com>
2021-05-01 23:18:02 +0900

    [refactor] 쉼표 구분 문자열 상수 추가
    * split 내에 "," 직접 입력에서 상수 DIVISION_TEXT 추가

--- src/main/java/Utils/CarUtil.java
+++ src/main/java/Utils/CarUtil.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarUtil {
+    private static final String DIVISION_TEXT = ",";
 
     public static List<String> divideByCommas(String names) {
-        return Arrays.asList(names.split(","));
+        return Arrays.asList(names.split(DIVISION_TEXT));
     }
 
 }
