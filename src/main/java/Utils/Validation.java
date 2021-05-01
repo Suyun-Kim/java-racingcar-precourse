2515729f85d7df388c6073fa9a64865380a5dbe9
김수윤 <darecaga1@gamil.com>
2021-05-02 00:30:13 +0900

    [feat] 자동차명 중복여부, 입력 여부 기능

--- src/main/java/Utils/Validation.java
+++ src/main/java/Utils/Validation.java
@@ -1,11 +1,13 @@
 package Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Validation {
 
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MIN_COUNT = 1;
     private static final int MAX_COUNT = 9;
-
 
     public static boolean checkNameLength(int nameLength) {
         return nameLength <= MAX_NAME_LENGTH;
@@ -15,5 +17,24 @@ public class Validation {
     public static boolean checkCountRange(int count) {
         return count >= MIN_COUNT && count <= MAX_COUNT;
 
+    }
+
+    public static boolean checkDuplicate(List<String> carNames) {
+
+        List<String> temp = new ArrayList<>();
+
+        for(String carName : carNames) {
+            if(temp.contains(carName)) {
+                return false;
+            }
+            temp.add(carName);
+        }
+
+        return true;
+
+    }
+
+    public static boolean checkIsEmptyCarName(String carNames) {
+        return !carNames.isEmpty();
     }
 }
--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -1,14 +1,42 @@
 import Utils.Validation;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationTest {
+
+    private List<String> carNames;
+
+    @BeforeEach
+    void setUp() {
+        carNames = new ArrayList<>();
+        carNames.add("싼타페");
+        carNames.add("쏘나타");
+        carNames.add("투싼");
+        carNames.add("K5");
+    }
+
 
     @Test
     void 자동차이름_5자_이내_테스트() {
         assertThat(Validation.checkNameLength(5)).isTrue();
         assertThat(Validation.checkNameLength(6)).isFalse();
 
+    }
+
+    @Test
+    void 자동차이름_중복_테스트() {
+        assertThat(Validation.checkDuplicate(carNames)).isTrue();
+    }
+
+    @Test
+    void 자동차이름_입력_여부_테스트() {
+        assertThat(Validation.checkIsEmptyCarName("싼타페,")).isTrue();
+        assertThat(Validation.checkIsEmptyCarName("")).isFalse();
     }
 
     @Test
