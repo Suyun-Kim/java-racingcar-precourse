0eb6ef0005a3ff6422416200a591d8cf10896460
김수윤 <darecaga1@gmail.com>
2021-05-04 00:15:47 +0900

    [feat] 경주 시작 기능

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,10 @@
 import Utils.CarUtil;
 
-public class Apllication {
+public class Application {
 
     public static void main(String[] args) {
 
-            Racing.startRacing();
+        Racing.startRacing();
 
     }
 
--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -1,19 +1,50 @@
 import Utils.CarUtil;
-import Utils.Validation;
-import car.Car;
+import Utils.CreateNumber;
+import Utils.RacingUtil;
+import vo.Car;
+import vo.UserNumber;
 
 import java.util.List;
 
 public class Racing {
 
     private static int userNumber = 0;
-    private static String carNames = "";
+    private static int maxMoveCount = 0;
     private static List<Car> cars;
+
 
     public static void startRacing() {
 
         cars = CarUtil.divideByCommas(CarUtil.inputCarNames());
+        userNumber = new UserNumber(CarUtil.inputUserNumber()).getUserNumber();
 
+        for(int i=0; i < userNumber; i++) {
+            for(Car car : cars) {
+
+                move(car);
+                System.out.println(RacingUtil.printOutGame(car));
+            }
+        }
+
+        getVictoryCars();
+        System.out.print(RacingUtil.printGameResult(cars));
+    }
+
+    public static void move(Car car) {
+        car.setMoveStatus(CarUtil.changeCarStatus(CreateNumber.generateRandomNumber()));
+        car.setMoveCount(CarUtil.addMoveCount(car));
 
     }
+
+    public static void getVictoryCars() {
+        maxMoveCount = RacingUtil.getMaxMoveCount(cars);
+        cars = RacingUtil.setVictoryStatus(cars, maxMoveCount);
+
+    }
+
+
+
+
+
+
 }
